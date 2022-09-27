6e1d3cae8aab7bdd2a14ff6cc788c932d1f4fbbe
Marty Nelson <marty@martypdx.com>
2022-09-27 15:23:59 -0700

    basic jsx and css worked out for form controls

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,5 +1,51 @@
 import styles from './Dashboard.css';
 
 export default function Dashboard() {
-  return <div className={styles.Dashboard}>Dashboard</div>;
+  return (
+    <div className={styles.Dashboard}>
+      <form>
+        <label>
+          Name
+          <input name="name" placeholder="your name" />
+        </label>
+
+        <label>
+          Date
+          <input
+            required
+            type="date"
+            name="date"
+            placeholder="pick a date"
+          />
+        </label>
+
+        <label>
+          Option
+          <select required>
+            <option disabled selected value="">
+              Select an Option...
+            </option>
+            <option value="1">A</option>
+            <option value="2">B</option>
+            <option value="3">C</option>
+          </select>
+        </label>
+
+        <label>
+          Bio
+          <textarea name="bio" placeholder="tell us about yourself" />
+        </label>
+
+        <fieldset>
+          <legend>Do you accept?</legend>
+          <label className={styles.OptionLabel}>
+            <input type="checkbox" />
+            Yes
+          </label>
+        </fieldset>
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
