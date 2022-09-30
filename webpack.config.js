03eaa6c3260dbeaebbde7f08227d2d41f223172e
Logan Barnett <logan@alchemycodelab.com>
2022-09-30 10:27:33 -0700

    use human friendly names when CSS module hashing
    This makes debugging CSS issues much easier.

--- webpack.config.js
+++ webpack.config.js
@@ -74,7 +74,9 @@ module.exports = {
             loader: 'css-loader',
             options: {
               sourceMap: true,
-              modules: true,
+              modules: {
+                localIdentName: '[name]__[local]__[hash:base64:5]',
+              },
               importLoaders: 1,
             },
           },
